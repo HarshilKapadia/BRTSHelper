0fd623427b4754297d19aa49976b1089171b8a55
hardik1410 <hardik19998@gmail.com>
2019-02-22 20:20:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TravellingActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TravellingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travelling2);
+    }
+}
